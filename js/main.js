340626cb3fa5b4b813fddfea98bc57bf2858cbe8
eric-rodwoods <sadinoke@gmail.com>
2020-04-25 16:50:44 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('.main-slider', {
+  speed: 800,
+  parallax: true,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
